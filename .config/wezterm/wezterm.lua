08ea407c78eb01b8e5f4695226e735351a3fb787
Chad Wade Day, Jr. <chad.day@medbridgeed.com>
2023-12-08 09:20:04 -0800

    update wezterm font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,6 @@ local wezterm = require 'wezterm'
 return {
   color_scheme = 'Brogrammer',
   enable_tab_bar = false,
-  font = wezterm.font 'ComicCodeLigatures NFM',
+  font = wezterm.font 'ComicCodeLigatures Nerd Font Mono',
   font_size = 24.0,
 }
