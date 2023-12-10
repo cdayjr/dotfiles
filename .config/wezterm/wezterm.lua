e8d7feca70a01616e10e3074b37c4264ed1e40f0
Chad Wade Day, Jr. <cdayjr@users.noreply.github.com>
2023-12-10 00:49:36 -0800

    show tab bar if more than one tab is active

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require 'wezterm'
 return {
   color_scheme = 'Brogrammer',
-  enable_tab_bar = false,
+  enable_tab_bar = true,
   font = wezterm.font 'ComicCodeLigatures Nerd Font Mono',
   font_size = 24.0,
+  hide_tab_bar_if_only_one_tab = true,
 }
